d3c1631adf1597810edf5eb8a40f9df4c5b50669
Lenny Gonzales <91269114+LennyGonzales@users.noreply.github.com>
2022-04-15 15:06:21 +0200

    modify ArithmecticTest1

--- tp1/src/ArithmeticTest1.java
+++ tp1/src/ArithmeticTest1.java
@@ -1,7 +1,7 @@
 /*
  * Test Arithmetic Operations
  */
-public class ArithmeticTest1 {     // Save as "ArithmeticTest1.java"
+public class ArithmeticTest1 {    // Save as "ArithmeticTest1.java"
    public static void main(String[] args) {
       int number1 = 98; // Declare an int variable number1 and initialize it to 98
       int number2 = 5;  // Declare an int variable number2 and initialize it to 5
